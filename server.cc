e1e8dbfd911f5cf1f5b832d784464dfdb035269d
Tiancheng Lou <loutiancheng@baidu.com>
2016-11-04 23:38:39 -0400

    Add model-interface.

--- server.cc
+++ server.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "model-interface.h"
 #include "server-interface.h"
 #include "unit-interface.h"
 
@@ -7,16 +8,22 @@ using namespace std;
 
 namespace open_bracket {
 
-void rank(const Request& request, Response* response) {
+void rank(const Request& request, const Model& model, Response* response) {
+	Unit unit;
+	unit.display_name = "downtown";
+	response->units.push_back(unit);
 }
 
 void start() {
+	// TODO: load model.
+	Model model;
+
 	// TODO: load request.
 	Request request;
 	Response response;
 
 	// TODO: write responses.
-	rank(request, &response);
+	rank(request, model, &response);
 	//cout << "reponse = " << response.DebugString().c_str();
 }
 
