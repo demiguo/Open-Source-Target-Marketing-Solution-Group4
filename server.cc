27a5e86b65339368635f22cafdde188360d7c1d5
Tiancheng Lou <loutiancheng@baidu.com>
2016-11-05 00:16:08 -0400

    Output download.

--- server.cc
+++ server.cc
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include "model-interface.h"
 #include "server-interface.h"
 #include "unit-interface.h"
+
+//#ifdef ENABLE_CGI
+
+#ifdef ENABLE_CGI
+#include <cgicc/CgiDefs.h> 
+#include <cgicc/Cgicc.h> 
+#include <cgicc/HTTPHTMLHeader.h> 
+#include <cgicc/HTMLClasses.h> 
+#endif
 
 using namespace std;
 
@@ -14,23 +24,40 @@ void rank(const Request& request, const Model& model, Response* response) {
 	response->units.push_back(unit);
 }
 
+void generate_html(const Response& response) {
+	string ret = "<html>\n";
+	ret += "  <head>\n";
+	ret += "    <title>Results</title>\n";
+	ret += "  </head>\n";
+	ret += "  <body>\n";
+	for (const auto& unit : response.units) {
+		ret += "    <p>";
+		ret += unit.display_name;
+		ret += "</p>\n";
+	}
+	ret += "  </body>\n";
+	ret += "</html>\n";
+	cout << ret << endl;
+}
+
 void start() {
 	// TODO: load model.
 	Model model;
 
-	// TODO: load request.
 	Request request;
-	Response response;
+#ifdef ENABLE_CGI
+	Cgicc formData;
+	// TODO: parse request from HTML.
+#endif
 
-	// TODO: write responses.
+	Response response;
 	rank(request, model, &response);
-	//cout << "reponse = " << response.DebugString().c_str();
+	generate_html(response);
 }
 
 }  // namespace 
 
-int main() {	
-	cout << "Start service.\n";
+int main() {
 	open_bracket::start();
 	return 0;
 }
