0f5ea45fdd2f190e243f456cdcf8eaf8a6759720
Tiancheng Lou <loutiancheng@baidu.com>
2016-11-05 00:19:03 -0400

    Add feature interface.

---
+++ feature-interface.h
@@ -0,0 +1,13 @@
+#ifndef FEATURE_INTERFACE_H
+#define FEATURE_INTERFACE_H
+
+#include <cinttypes>
+#include <cstdint>
+#include <string>
+
+struct Feature {
+	Unit unit;
+	double label;
+};
+
+#endif  // FEATURE_INTERFACE_H
